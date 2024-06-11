29e3173c964521f3168fa4ff45b2857456daaf5f
dciancu <daniel@iancu.io>
2024-06-11 11:37:27 +0300

    Fixed GitHub action

--- download.sh
+++ download.sh
@@ -5,6 +5,20 @@ set -euo pipefail
 SCRIPT_DIR="$(dirname "$0")"
 cd "$SCRIPT_DIR"
 
+if ! command -v jq &>/dev/null; then
+    echo 'ERROR: jq command not found, please install jq'
+    return 1
+elif ! command -v whois &>/dev/null; then
+    echo 'ERROR: whois command not found, please install whois'
+    return 1
+elif ! command -v sort &>/dev/null; then
+    echo 'ERROR: sort command not found, please install sort'
+    return 1
+elif ! command -v uniq &>/dev/null; then
+    echo 'ERROR: uniq command not found, please install uniq'
+    return 1
+fi
+
 
 DROP_V4='https://www.spamhaus.org/drop/drop_v4.json'
 DROP_V6='https://www.spamhaus.org/drop/drop_v6.json'
